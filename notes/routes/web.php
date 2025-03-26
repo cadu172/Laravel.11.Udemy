8c07d2bc9c96feaaf4ceb9b9573efbe1ffdaba87
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2025-03-26 08:52:05 -0300

    Seção 5: Primeiro Projeto Completo Laravel - Bloco de Notas | 35. Apresentar Uma View a Partir de um Controller

--- notes/routes/web.php
+++ notes/routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    echo "Hello World";
 });
+
+Route::get('/about', function() {
+    return "About Us";
+});
+
+
+Route::get('/main', [MainController::class, 'index']);
