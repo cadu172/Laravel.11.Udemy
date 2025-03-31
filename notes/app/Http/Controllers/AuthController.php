0aa0017cc94a74ef031aecf59e4be0c58652f352
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2025-03-30 23:12:57 -0300

    Seção 5: Primeiro Projeto Completo Laravel - Bloco de Notas | 46. Definir Textos Personalizados para os Erros de Validação

--- notes/app/Http/Controllers/AuthController.php
+++ notes/app/Http/Controllers/AuthController.php
@@ -13,10 +13,19 @@ class AuthController extends Controller
 
     public function loginSubmit(Request $request) {
 
-        $request->validate([
-            "text_username" => "required",
-            "text_password" => "required"
-        ]);
+        $request->validate(
+            [
+                "text_username" => "required|email",
+                "text_password" => "required|min:8|max:20",
+            ],
+            [
+                "text_username.required" => "O campo e-mail não foi preenchido, este campo é obrigatório",
+                "text_username.email" => "O campo e-mail não é um e-mail válido",
+                "text_password.required" => "O campo senha não foi preenchido, este campo é obrigatório",
+                "text_password.min" => "O tamanho mínimo do password é :min caracteres",
+                "text_password.max" => "O tamanho maximo do password é :max caracteres",
+            ]
+        );
 
         return "OK";
 
