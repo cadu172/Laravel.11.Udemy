79e9ed0c0472f435ee0cd73af1019afb09b3dc38
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2025-04-04 12:16:06 -0300

    Seção 5: Primeiro Projeto Completo Laravel - Bloco de Notas | 54. Introdução ao Middleware

--- notes/app/Http/Controllers/MainController.php
+++ notes/app/Http/Controllers/MainController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
+    public function index()
+    {
+        return "main.index";
+    }
+
+    public function newNote()
+    {
+        return "Main controller.newNote";
+    }
 
 }
--- notes/routes/web.php
+++ notes/routes/web.php
@@ -3,7 +3,14 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
+use \App\Http\Middleware\CheckIsLogged;
 
-Route::GET("/login",[AuthController::class,"login"]);
+Route::GET("/login",[AuthController::class,"login"])->name("login");
 Route::POST("/loginSubmit",[AuthController::class,"loginSubmit"]);
-Route::GET("/logout",[AuthController::class,"logout"]);
+
+
+Route::middleware(CheckIsLogged::class)->group(function () {
+    Route::GET("/logout",[AuthController::class,"logout"])->name("logout");    
+    Route::GET("/newNote",[MainController::class,"newNote"])->name("main.newNote"); 
+    Route::GET("/",[MainController::class,"index"])->name("main.index"); 
+});
