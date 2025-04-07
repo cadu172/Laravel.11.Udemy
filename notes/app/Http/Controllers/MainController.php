b6ddaeaea7d666a01569365082c6b3645c7054fb
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2025-04-07 08:17:10 -0300

    Seção 5: Primeiro Projeto Completo Laravel - Bloco de Notas | 59. Carregando As Notas do User Logado

--- notes/app/Http/Controllers/MainController.php
+++ notes/app/Http/Controllers/MainController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class MainController extends Controller
 {
     public function index()
     {
+
+        $user_id = session("user.id");
+
+        $user = User::find($user_id)->toArray();
+
+        dd(User::find($user_id)->notes->toArray());
+
+        die();
+
         return view("home");
     }
 
--- notes/app/Models/User.php
+++ notes/app/Models/User.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Note;
 
 class User extends Model
 {
     protected $table = 'public.users';
+
+    public function notes() {
+        return $this->hasMany(Note::class);
+    }
 }
