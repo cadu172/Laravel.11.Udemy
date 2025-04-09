ea8b2514b5c8653d34e8f20dbf4a336e89cb3592
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2025-04-09 08:43:54 -0300

    Seção 5: Primeiro Projeto Completo Laravel - Bloco de Notas | 62. Capturando e Validando IDs Encriptados

--- notes/app/Http/Controllers/MainController.php
+++ notes/app/Http/Controllers/MainController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Contracts\Encryption\DecryptException;
+use Illuminate\Support\Facades\Crypt;
 
 class MainController extends Controller
 {
@@ -19,6 +21,29 @@ class MainController extends Controller
     public function newNote()
     {
         return "Main controller.newNote";
+    }
+
+    public function editNote($id)
+    {
+
+        try {
+            $id = Crypt::decrypt($id);
+        }
+        catch (DecryptException $e ) {
+            return redirect()->route("home");
+        }
+
+        return "Main controller.editNote: " . $id;
+    }
+
+    public function deleteNote($id) {
+        try {
+            $id = Crypt::decrypt($id);
+        }
+        catch (DecryptException $e ) {
+            return redirect()->route("home");
+        }
+        return "Main controller.deleteNote: " . $id;
     }
 
 }
--- notes/routes/web.php
+++ notes/routes/web.php
@@ -11,14 +11,16 @@ use App\Http\Middleware\CheckIsNotLogged;
  */
 Route::middleware(CheckIsNotLogged::class)->group(function() {
     Route::GET("/login",[AuthController::class,"login"])->name("login");
-    Route::POST("/loginSubmit",[AuthController::class,"loginSubmit"]);    
+    Route::POST("/loginSubmit",[AuthController::class,"loginSubmit"]);
 });
 
 /**
  * Rotas de usuários autenticados
  */
 Route::middleware(CheckIsLogged::class)->group(function () {
-    Route::GET("/logout",[AuthController::class,"logout"])->name("logout");    
-    Route::GET("/newNote",[MainController::class,"newNote"])->name("new"); 
-    Route::GET("/",[MainController::class,"index"])->name("home"); 
+    Route::GET("/logout",[AuthController::class,"logout"])->name("logout");
+    Route::GET("/newNote",[MainController::class,"newNote"])->name("new");
+    Route::GET("/editNote/{id}",[MainController::class,"editNote"])->name("edit");
+    Route::GET("/deleteNote/{id}",[MainController::class,"deleteNote"])->name("delete");
+    Route::GET("/",[MainController::class,"index"])->name("home");
 });
