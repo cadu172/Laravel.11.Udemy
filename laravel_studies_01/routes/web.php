f05e9578c93033b27f72f7cf076d54b69a69cd80
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2025-04-14 08:45:30 -0300

    Seção 6: Routes, Controllers e Middleware em Maior Detalhe | 80. Single Action Controller e Resource Controller

--- laravel_studies_01/routes/web.php
+++ laravel_studies_01/routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\ClientsController;
 use App\Http\Controllers\MainController;
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\SingleActionController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/init",[MainController::class,"initMethod"])->name("init");
 Route::get("/view",[MainController::class,"viewPage"])->name("view");
+
+// rota para single action controller
+Route::get("/singleAtionController",SingleActionController::class)->name("single");
+
+// rota para controller com construtor
+//Route::resource("users",UserController::class);
+
+Route::resources([
+    "products" => ProductsController::class,
+    "users" => UserController::class,
+    "clients" => ClientsController::class
+]);
