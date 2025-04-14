1152f405c0cf215a1c27226a84b1e9184741a8ea
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2025-04-13 21:51:21 -0300

    Seção 6: Routes, Controllers e Middleware em Maior Detalhe | 79. Consolidação Sobre Controllers

--- laravel_studies_01/app/Http/Controllers/MainController.php
+++ laravel_studies_01/app/Http/Controllers/MainController.php
@@ -3,47 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class MainController extends Controller
 {
-    public function index()
-    {
-        return 'MainController.index';
+    public function initMethod(): string {
+        return "Hello from MainController (initMethod)";
     }
 
-    public function about()
-    {
-        return 'MainController.about';
+    public function viewPage() : View {
+        return view("home");
     }
-
-    public function rotaComParametro($id)
-    {
-        return '(MainController.rotaComParametro) Rota com parametro: ' . $id;
-    }
-
-    public function rotaComDoisParametros($value1, $value2)
-    {
-        return '(MainController.rotaComDoisParametros) Rota com dois parametros: valor1 => ' . $value1 . " e valor2 => " . $value2;
-    }
-
-    public function rotaComDoisParametrosComRequest(Request $request, $value1, $value2)
-    {
-        return '(MainController.rotaComDoisParametrosComRequest) Rota com dois parametros: valor1 => ' . $value1 . " e valor2 => " . $value2;
-    }
-
-    public function rotaComParametroOpcional($value1 = null)
-    {
-        return '(MainController.rotaComParametroOpcional) Rota com parametro opcional: ' . $value1;
-    }
-
-    public function rotaComParametroObrigatorioOpcional($value1, $value2 = null)
-    {
-        return '(MainController.rotaComParametroObrigatorioOpcional) Rota com parametro obrigatorio e opcional: ' . $value1 . ' e ' . $value2;
-    }
-
-    public function showUserPost($user_id, $post_id)
-    {
-        return '(MainController.showUserPost) User ID: ' . $user_id . ' Post ID: ' . $post_id;
-    }
-
 }
