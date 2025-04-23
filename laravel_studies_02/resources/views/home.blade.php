dcc03697e8f5b5c290d65a0222a5e11ac6321d0f
WSL Ubuntu 20.04 - Empresa <cadu172@gmail.com>
2025-04-23 19:34:28 -0300

    Seção 7: Views e Blade Template Engine | 94. @continue, @break e Loop Variable

--- laravel_studies_02/resources/views/home.blade.php
+++ laravel_studies_02/resources/views/home.blade.php
@@ -2,31 +2,36 @@
 @section('content')
 
 {{-- estrutura for --}}
-@for ($index=0;$index<5;$index++)
-    <h1>{{$index}}</h1>
+@for ($index=0;$index<10;$index++)
+
+    {{-- continue no indice 5 (pular este indice) --}}
+    @if ($index==5)
+        @continue
+    @endif
+
+    <p>{{$index}}</p>
+
+    {{-- break no indice 7 --}}
+    @if ($index==7)
+        @break
+    @endif
+
 @endfor
 
 <hr />
 
-{{-- Estrutura Foreach --}}
-@foreach ($cities as $citie)
-    <h1>{{$citie}}</h1>
+@foreach ($cities as $city)
+    <p>
+        {{$loop->index . " - " . $city}}
+        @if ($loop->first)
+            <strong> (Primeira Cidade)</strong>
+        @elseif ($loop->last)
+            <strong> (Última Cidade)</strong>
+        @endif
+
+    </p>
 @endforeach
 
 <hr />
-
-{{-- Estrutura Forelse --}}
-@forelse ($names as $name)
-    <h1>{{$name}}</h1>
-@empty
-    <h1>$names não encontrado ou vazio</h1>
-@endforelse
-
-<hr />
-
-{{--  Estrutura while --}}
-@while ($indice < 10)
-    <h1>{{$indice++}}</h1>
-@endwhile
 
 @endsection
