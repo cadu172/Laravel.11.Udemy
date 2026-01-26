749d5cda2bb2c700536e0e0cd4d29942c22ee2f4
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2026-01-25 22:44:32 -0300

    Seção 9: Blade Template Engine, Blade Components e Slots | 118. Componentes com Vários Slots

---
+++ laravel_studies_03/resources/views/components/multi-slot.blade.php
@@ -0,0 +1,8 @@
+<div class="card col mt-5 me-2" style="width: 18rem;">
+  <h2>Componente com vários SLOTS</h2>
+  <div class="card-body">
+    <p>{{ $title }}</p>
+    <p>{{ $content }}</p>
+    <p>{{ $footer }}</p>
+  </div>
+</div>
--- laravel_studies_03/resources/views/home.blade.php
+++ laravel_studies_03/resources/views/home.blade.php
@@ -8,9 +8,24 @@
             <x-card-pessoa :$pessoa :$idiomas  />
         @endforeach
         --}}
-        <x-other-card>
+        {{--<x-other-card>
             <h1 class="text-danger">Aqui estou passando o conteúdo do Slot</h1>
-        </x-other-card>
+        </x-other-card>--}}
+
+
+        <x-multi-slot>
+            <x-slot:title>Título passado ao Slot</x-slot:title>
+            <x-slot:content>Conteúdo do Slot</x-slot:content>
+            <x-slot:footer>
+                <p>Footer do Slot</p>
+                <hr />
+                <ul>                
+                    <li>Linha 1</li>
+                    <li>Linha 2</li>
+                    <li>Linha 3</li>
+                </ul>
+            </x-slot:footer>
+        </x-multi-slot>
     </div>
 </div>
 @endsection
