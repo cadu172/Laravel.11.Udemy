33e1b47a1b73460a20f6664f5685739959cd245e
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2026-01-25 22:30:32 -0300

    Seção 9: Blade Template Engine, Blade Components e Slots | 117. Introdução aos Slots

--- laravel_studies_03/resources/views/home.blade.php
+++ laravel_studies_03/resources/views/home.blade.php
@@ -3,9 +3,14 @@
 @section('content')
 <div class="container">
     <div class="row">
+        {{-- 
         @foreach ($pessoas as $pessoa=>$idiomas)
             <x-card-pessoa :$pessoa :$idiomas  />
         @endforeach
+        --}}
+        <x-other-card>
+            <h1 class="text-danger">Aqui estou passando o conteúdo do Slot</h1>
+        </x-other-card>
     </div>
 </div>
 @endsection
