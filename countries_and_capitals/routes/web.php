5a834bec285dfb0d9b174084ef6e6053be9c28a9
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2026-01-26 07:47:28 -0300

    Seção 10: Projeto Completo - Countries & Capitais | 123. Assets, Dados Estáticos e Controller

--- countries_and_capitals/routes/web.php
+++ countries_and_capitals/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return "Hello World! Projeto Countries and Capitals";
 });
+
+
+Route::get('/app_data', [MainController::class, 'showData']);
