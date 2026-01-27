23f4aabf0cba6b92110919b1c8562e6e17a759f3
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2026-01-27 08:17:29 -0300

    Seção 10: Projeto Completo - Countries & Capitais | 124. Blade Layout com Components

---
+++ countries_and_capitals/resources/views/home.blade.php
@@ -0,0 +1,3 @@
+<x-main-layout pageTitle="Título da Página">
+    <h1 class="text-center my-5">Home Page Teste</h1>
+</x-main-layout>
--- countries_and_capitals/routes/web.php
+++ countries_and_capitals/routes/web.php
@@ -3,9 +3,5 @@
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return "Hello World! Projeto Countries and Capitals";
-});
-
-
+Route::view('/', 'home');
 Route::get('/app_data', [MainController::class, 'showData']);
