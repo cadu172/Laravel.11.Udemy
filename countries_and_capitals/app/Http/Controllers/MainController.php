f606147065b0829144521dfb56b29c5d04504188
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2026-01-27 08:43:23 -0300

    Seção 10: Projeto Completo - Countries & Capitais | 125. Vamos Preparar o Jogo - parte 1

--- countries_and_capitals/app/Http/Controllers/MainController.php
+++ countries_and_capitals/app/Http/Controllers/MainController.php
@@ -13,8 +13,37 @@ class MainController extends Controller
         $this->app_data = require(app_path('app_data.php'));
     }
 
-    public function showData()
+    public function startGame()
     {
-        return response()->json($this->app_data, 200);
+        return view('home');
+    }
+
+    public function prepareGame(Request $request)
+    {
+        $request->validate(
+            // constaints
+            [
+                'total_questions' => 'required|integer|min:3|max:30'
+            ],
+            // messages
+            [
+                'total_questions.required' => 'O número de questões deve ser informado',
+                'total_questions.integer' => 'O Campo com o número de questões deve ser um valor inteiro',
+                'total_questions.min' => 'O Número mínimo de questões é :min',
+                'total_questions.max' => 'O Número máximo de questões é :max'
+            ],
+        );
+
+        // numero total de questões, se não informado inicializar com 1
+        $total_questions = $request->integer('total_questions', 1);
+
+        $quiz = $this->prepareQuiz($total_questions);
+
+        dd($quiz);
+
+    }
+
+    private function prepareQuiz($total_questions) {
+        return $total_questions;
     }
 }
