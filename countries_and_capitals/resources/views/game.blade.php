701915168a427dcea410feb5182976f9536469b3
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2026-02-01 23:06:24 -0300

    Seção 10: Projeto Completo - Countries & Capitais | 128. Apresentação da Questão com Componentes - parte 2

--- countries_and_capitals/resources/views/game.blade.php
+++ countries_and_capitals/resources/views/game.blade.php
@@ -1,3 +1,19 @@
-<div>
-    <!-- It is not the man who has too little, but the man who craves more, that is poor. - Seneca -->
-</div>
+<x-main-layout pageTitle="Quiz Countries and Capitals">
+    <div class="container">
+
+        <x-question :currentQuestion="$currentQuestion" :totalQuestions="$totalQuestions" :country="$country" />
+
+        <div class="row">
+            @foreach ($answers as $answer)
+                <x-answer :capital="$answer" />
+            @endforeach
+        </div>
+        
+    </div>
+
+    <!-- cancel game -->
+    <div class="text-center mt-5">
+        <a href="#" class="btn btn-outline-danger mt-3 px-5">CANCELAR JOGO</a>
+    </div>
+
+</x-main-layout>
