12b4d429abb532ef4bd89db99947667edee3fd0d
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2026-02-08 22:48:59 -0300

    Seção 11: Bases de Dados, Migrations e Seeding | 134. Criando e Conectando a uma Base de Dados MySQL

--- laravel_studies_04/routes/web.php
+++ laravel_studies_04/routes/web.php
@@ -1,5 +1,23 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/','home');
+
+//Route::view('/','home');
+
+Route::get('/', function() {
+
+    try {
+        
+        DB::connection()->getPdo();
+
+        dd("Conexão realizada com sucesso: " . DB::connection()->getDatabaseName());
+
+    } catch (\Exception $e) {
+        
+        dd("Erro ao conectar-se: " . $e->getMessage());
+
+    }
+
+});
