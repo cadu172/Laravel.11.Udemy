a323e0a92a25dc6719a3fc2f92b89dec833fd28b
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2026-02-19 07:28:45 -0300

    Seção 12: Executando Queries com Laravel Query Builder | 151. Queries com Query Builder - Parte 1

--- laravel_studies_05/app/Http/Controllers/MainController.php
+++ laravel_studies_05/app/Http/Controllers/MainController.php
@@ -13,12 +13,46 @@ class MainController extends Controller
      */
     public function index()
     {
-        $data = DB::table('clients')->get();
-        //$this->showRawData($data);
-        $this->showTableData($data);
+        // 1 - executa um select * na tabela clients
+        //$clients = DB::table('clients')->get();
+
+        // 2 - converte o model e array
+        //$clients = DB::table('clients')->get()->toArray();
+
+        // foreach recursivo e converte cada item da collection em um array
+        /*$clients = DB::table('clients')->get()->map(function($item){
+            return (array) $item;
+        });*/
+
+        // lista somente colunas especificas da tabela retornada pela facade DB
+        //$products = DB::table('products')->get(['id','product_name','price']);
+
+        // pluck retorna uma collection contendo apenas a coluna especificada
+        //$results = DB::table('products')->pluck('product_name');
+
+        // retorna somente a primera linha da query
+        /*$results = DB::table('products')
+            ->get()
+            ->first();*/
+
+        // retorna somente a ultima linha da query
+        /*$results = DB::table('products')
+            ->get()
+            ->last();*/
+
+        // retorna um id especifico, no exemplo o id 23
+        $results = DB::table('products')->find(23);
+
+        // imprime o conteudo da collection em formato de tabela
+        //$this->showTableData($products);
+
+        // imprime o conteudo da tabela clients em formato de tabela
+        $this->showRawData($results);
+
+
     }
 
-    private function showRawData(Collection $p_Data) {
+    private function showRawData($p_Data) {
         echo '<pre>';
         print_r($p_Data);
         echo '</pre>';
